26ebf4fb294832349a8a56dfe5479389c1367ead
Shyvee <xshi2016@kellogg.northwestern.edu>
2015-05-06 17:39:38 -0500

    up to edit form
    up to edit form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+  end
+
+  def create_row
+    z = Photo.new
+    z.source = params["source"]
+    z.caption = params["caption"]
+    z.save
+  end
+
+  def destroy
+    z = Photo.find(params["id"])
+    z.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
   def show
    @photo_id = Photo.find(params["id"])
+  end
+
+  def edit_form
+    z = Photo.new
+    z.source = params["source"]
+    z.caption = params["caption"]
+    z.save
   end
 
 end
